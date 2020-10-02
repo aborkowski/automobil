e8de3e3f52fe2522feb58c3f4ebb111e0a462db3
Alexander Borkowski <alex-b@opus-b.com>
2020-10-02 19:11:18 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -51,112 +51,10 @@ function lights_6 () {
 }
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showNumber(1)
-    if (receivedNumber == 1) {
-        basic.showNumber(4)
-        radio2(receivedNumber)
-    } else if (receivedNumber == 2) {
-        radio2(receivedNumber)
-    } else if (receivedNumber == 3) {
-        radio2(receivedNumber)
-    } else if (receivedNumber == 4) {
-        radio2(receivedNumber)
-    } else if (receivedNumber == 5) {
-        radio2(receivedNumber)
-    } else if (receivedNumber == 6) {
-        radio2(receivedNumber)
-    } else if (receivedNumber == 7) {
-        radio2(receivedNumber)
-    } else if (receivedNumber == 8) {
+    if (receivedNumber == 8) {
         basic.showNumber(2)
         Variables()
-    }
-})
-function lights_1 () {
-    bitbot.setPixelColor(0, 0x659900)
-    bitbot.setPixelColor(6, 0x659900)
-    basic.pause(200)
-    bitbot.ledClear()
-}
-function Snabbare () {
-    mode = Speed + 10
-}
-function Lights_Backward () {
-    for (let index = 0; index < 2; index++) {
-        Lights2()
-    }
-}
-function Lights_forward () {
-    for (let index = 0; index < 2; index++) {
-        Lights()
-    }
-}
-function lights_4 () {
-    bitbot.setPixelColor(3, 0x659900)
-    bitbot.setPixelColor(9, 0x659900)
-    basic.pause(200)
-    bitbot.ledClear()
-}
-function lights_3 () {
-    bitbot.setPixelColor(2, 0x659900)
-    bitbot.setPixelColor(8, 0x659900)
-    basic.pause(200)
-    bitbot.ledClear()
-}
-input.onButtonPressed(Button.A, function () {
-    radio2(1)
-})
-function Line_followers () {
-    for (let index = 0; index < 1e+103; index++) {
-        if (bitbot.readLine(BBLineSensor.Left) == 1) {
-            bitbot.driveTurnMilliseconds(BBRobotDirection.Left, 300, 45)
-            Lights_While_Line_folowers_on()
-        } else if (bitbot.readLine(BBLineSensor.Right) == 1) {
-            bitbot.driveTurnMilliseconds(BBRobotDirection.Right, 300, 45)
-            Lights_While_Line_folowers_on()
-        } else if (bitbot.readLine(BBLineSensor.Left) == 0 == (bitbot.readLight(BBLightSensor.Left) == 0)) {
-            bitbot.drive(Speed)
-        }
-    }
-}
-function Lights () {
-    lights_1()
-    lights_2()
-    lights_3()
-    lights_4()
-    lights_5()
-    lights_6()
-}
-function Turn_Left () {
-    bitbot.stop(BBStopMode.Brake)
-    for (let index = 0; index < 2; index++) {
-        basic.showLeds(`
-            . . . . .
-            . # . . .
-            # # # . .
-            . # # . .
-            . . # . .
-            `)
-        for (let index = 0; index <= 5; index++) {
-            Led = index + 0
-            bitbot.setPixelColor(Led, 0xFFC000)
-            basic.pause(200)
-            bitbot.ledClear()
-        }
-        bitbot.driveTurnMilliseconds(BBRobotDirection.Left, 300, 200)
-    }
-    basic.pause(1000)
-    bitbot.stop(BBStopMode.Brake)
-}
-function Lights2 () {
-    lights_6()
-    lights_5()
-    lights_4()
-    lights_3()
-    lights_2()
-    lights_1()
-}
-function radio2 (receivedNumber: number) {
-    while (lock == 1) {
+    } else if (lock == 1) {
         basic.showNumber(5)
         if (receivedNumber == 1) {
             if (mode == 1) {
@@ -230,6 +128,90 @@ function radio2 (receivedNumber: number) {
             }
         }
     }
+})
+function lights_1 () {
+    bitbot.setPixelColor(0, 0x659900)
+    bitbot.setPixelColor(6, 0x659900)
+    basic.pause(200)
+    bitbot.ledClear()
+}
+function Snabbare () {
+    mode = Speed + 10
+}
+function Lights_Backward () {
+    for (let index = 0; index < 2; index++) {
+        Lights2()
+    }
+}
+function Lights_forward () {
+    for (let index = 0; index < 2; index++) {
+        Lights()
+    }
+}
+function lights_4 () {
+    bitbot.setPixelColor(3, 0x659900)
+    bitbot.setPixelColor(9, 0x659900)
+    basic.pause(200)
+    bitbot.ledClear()
+}
+function lights_3 () {
+    bitbot.setPixelColor(2, 0x659900)
+    bitbot.setPixelColor(8, 0x659900)
+    basic.pause(200)
+    bitbot.ledClear()
+}
+input.onButtonPressed(Button.A, function () {
+	
+})
+function Line_followers () {
+    for (let index = 0; index < 1e+103; index++) {
+        if (bitbot.readLine(BBLineSensor.Left) == 1) {
+            bitbot.driveTurnMilliseconds(BBRobotDirection.Left, 300, 45)
+            Lights_While_Line_folowers_on()
+        } else if (bitbot.readLine(BBLineSensor.Right) == 1) {
+            bitbot.driveTurnMilliseconds(BBRobotDirection.Right, 300, 45)
+            Lights_While_Line_folowers_on()
+        } else if (bitbot.readLine(BBLineSensor.Left) == 0 == (bitbot.readLight(BBLightSensor.Left) == 0)) {
+            bitbot.drive(Speed)
+        }
+    }
+}
+function Lights () {
+    lights_1()
+    lights_2()
+    lights_3()
+    lights_4()
+    lights_5()
+    lights_6()
+}
+function Turn_Left () {
+    bitbot.stop(BBStopMode.Brake)
+    for (let index = 0; index < 2; index++) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            # # # . .
+            . # # . .
+            . . # . .
+            `)
+        for (let index = 0; index <= 5; index++) {
+            Led = index + 0
+            bitbot.setPixelColor(Led, 0xFFC000)
+            basic.pause(200)
+            bitbot.ledClear()
+        }
+        bitbot.driveTurnMilliseconds(BBRobotDirection.Left, 300, 200)
+    }
+    basic.pause(1000)
+    bitbot.stop(BBStopMode.Brake)
+}
+function Lights2 () {
+    lights_6()
+    lights_5()
+    lights_4()
+    lights_3()
+    lights_2()
+    lights_1()
 }
 input.onButtonPressed(Button.AB, function () {
     Variables()
@@ -255,7 +237,7 @@ function lights_2 () {
     bitbot.ledClear()
 }
 input.onButtonPressed(Button.B, function () {
-    radio2(1)
+	
 })
 function driveForward () {
     Lights_forward()
@@ -322,7 +304,7 @@ function Lights_While_Line_folowers_on () {
 }
 function Variables () {
     basic.showNumber(3)
-    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
     Drive_Backwards_Left = 0
     Backwards = 0
     Drive_Backwards_Rights = 0
@@ -339,15 +321,15 @@ function Variables () {
 let Line = 0
 let Drive_Forwords_Left = 0
 let Drive_Backwards_Left = 0
+let Speed = 0
 let Fjärrstyr = 0
 let Drive_Backwards_Rights = 0
 let Drive_Forwords_Rights = 0
 let Halt = 0
 let Backwards = 0
 let Drive_Forwords = 0
-let lock = 0
-let Speed = 0
 let mode = 0
+let lock = 0
 let Led = 0
 radio.setGroup(1)
 I()
